897a8cc3a13c7b0fa4f165690536bba7ce5d2c44
Aru Toxeitova <a_tokseitova@kbtu.kz>
2025-09-18 20:27:02 +0500

    added casting

---
+++ w3school/casting.py
@@ -0,0 +1,28 @@
+#casting - менять типы данных
+#меняем на int
+x = int(8)
+y = int(2.2)
+z = int("7")
+print(x)
+print(y)
+print(z)
+
+#меняем на float
+x = float(9)
+y = float(9.2)
+z = float("5")
+w = float("99.9")
+print(x)
+print(y)
+print(z)
+print(w)
+
+#меняем на strings
+x = str("lala")
+y = str(8)
+z = str(0)
+print(x)
+print(y)
+print(z)
+print(x+y)
+
